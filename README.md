466095700ba699454a865784c8b6bbffd676cfdc
Jackson Lopes <jacksonlopes@gmail.com>
2019-07-19 19:45:10 -0300

    Commit inicial.

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
 # ckac
 Shell script para gerar um relatório simples com informações sobre ganho/perda de ativos da bolsa.
+
+**Formato do arquivo de configuração**
+
+	URL_<ATIVO>="código do ativo da URI" 
+	<ATIVO>=(
+		<NÚMERO AÇÕES>:<PREÇO COMPRA>
+		<NÚMERO AÇÕES>:<PREÇO COMPRA>
+	)
+
+Para obter o código do ativo:
+
+01 - Acesse o link da URI_PRINCIPAL
+
+02 - Escolha um papel
+
+03 - Recorte o trecho na url que corresponda após a string 'stockdetails' e anterior ao '?'
+
+04 - Adicione os dados no arquivo de configuração no formato apresentado acima.
+
+**Obs.: O script automaticamente identifica o novo papel e gera as informações pertinentes.**
+
+----
+
+**Exemplo: ckac.conf**
+
+----
+	URI_PRINCIPAL="https://www.msn.com/pt-br/dinheiro/stockdetails/"
+
+	URL_VALE3="bsp-vale3/fi-apnjsm" 
+	VALE3=(
+		2500:15.30
+		900:40.70
+	)
+
+	URL_PETR4="bsp-petr4/fi-apn4gh"
+	PETR4=(
+		100:19.70
+		800:55
+		300:15.40
+	)
+
+	URL_SLCE3="bsp-slce3/fi-apncjc"
+	SLCE3=(
+		500:10.10
+		800:22.60
+	)
+
+----
+
+**Exemplo de output**
+
+---
+jsl]$ ./ckac.sh 
+
+****************************************** VALE3 ******************************************
+
+QTDE: 2500  | VL_COMPRA: 15.30 == 38250.00  | COTAÇÃO: 52.72 == 131800.00 | GANHO DE 93550.00    | % 244.00
+
+QTDE: 900   | VL_COMPRA: 40.70 == 36630.00  | COTAÇÃO: 52.72 == 47448.00  | GANHO DE 10818.00    | % 29.00
+
+
+NÚM. AÇÕES: 3400
+
+TOTAL INVESTIDO:  74880.00
+
+TOTAL ATUAL:     179248.00
+
+DIFERENÇA:       104368.00
+
+
+****************************************** PETR4 ******************************************
+
+QTDE: 100   | VL_COMPRA: 19.70 == 1970.00   | COTAÇÃO: 27.44 == 2744.00   | GANHO DE 774.00      | % 39.00
+
+QTDE: 800   | VL_COMPRA: 55    == 44000     | COTAÇÃO: 27.44 == 21952.00  | ==> PERDA DE 22048.00 | % 50.00
+
+QTDE: 300   | VL_COMPRA: 15.40 == 4620.00   | COTAÇÃO: 27.44 == 8232.00   | GANHO DE 3612.00     | % 78.00
+
+NÚM. AÇÕES: 1200
+
+TOTAL INVESTIDO:  50590.00
+
+TOTAL ATUAL:      32928.00
+
+DIFERENÇA:       -17662.00
+
+
+****************************************** SLCE3 ******************************************
+
+QTDE: 500   | VL_COMPRA: 10.10 == 5050.00   | COTAÇÃO: 17.25 == 8625.00   | GANHO DE 3575.00     | % 70.00
+
+QTDE: 800   | VL_COMPRA: 22.60 == 18080.00  | COTAÇÃO: 17.25 == 13800.00  | ==> PERDA DE 4280.00 | % 23.00
+
+NÚM. AÇÕES: 1300
+
+TOTAL INVESTIDO:  23130.00
+
+TOTAL ATUAL:      22425.00
+
+DIFERENÇA:         -705.00
+
+
---
+++ ckac.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Jackson Lopes <jacksonlopes@gmail.com>
+# URL: https://jslabs.cc
+# Info/Source: 
+
+# Dependências:
+# binutils, curl
+
+function validar_ambiente() {
+   [ ! -f ckac.conf ] && {
+     echo "Erro: Arquivo 'ckac.conf' não encontrado."
+     exit 1
+   }
+   which curl 1>/dev/null
+   [ $? != 0 ] && {
+      echo "Erro: curl não encontrado..."
+      exit 1
+   }
+}
+
+source ckac.conf
+retorno=""
+
+function obter_cotacao() {
+  local cotacao=`curl -s $1 | grep 'data-role="currentvalue"' | cut -d'>' -f2- | cut -d'<' -f1`
+  echo $cotacao | tr ',' '.'  
+}
+
+function calcular_valores() {
+
+    local total_somatorio_compra=0
+    local total_somatorio_atual=0
+    local total_acoes=0
+
+    qtde=`echo $4 | cut -d':' -f1`
+    vl_compra=`echo $4 | cut -d':' -f2`
+    total_compra=$(bc <<< "$qtde * $vl_compra")    
+    total_atual=$(bc <<< "$qtde * $cotacao")        
+    
+    total_somatorio_compra=$(bc <<< "$1 + $total_compra")
+    total_somatorio_atual=$(bc <<< "$2 + $total_atual")
+    total_acoes=$(bc <<< "$3 + $qtde")    
+    
+    somatorio=$(bc <<< "$total_atual - $total_compra")
+    
+    echo $somatorio | grep "-" 1>/dev/null
+    if [ $? != 0  ]; then
+       imp_str="GANHO DE $somatorio"
+    else
+       somatorio=$(bc <<< "$somatorio * -1")
+       imp_str="==> PERDA DE $somatorio"
+    fi    
+    
+    # ( valor_inicial - valor_final ) / valor_inicial * 100        
+    perc=`echo "scale=2; ($total_compra - $total_atual) / $total_compra * 100" | bc`    
+    echo $perc | grep "-" 1>/dev/null
+    [ $? = 0 ] && {
+       perc=$(bc <<< "$perc * -1")
+    }    
+    
+    printf "QTDE: %-5s | VL_COMPRA: %-5s == %-9s | COTAÇÃO: %-5s == %-9s | %-20s | %% %s\n" "$qtde" "$vl_compra" "$total_compra" "$cotacao" "$total_atual" "$imp_str" "$perc"
+    perc=0
+    retorno="$total_somatorio_compra;$total_somatorio_atual;$total_acoes"
+}
+
+function imprimir_final() {
+  diferenca=$(bc <<< "$2 - $1")
+  echo "================="
+  echo "NÚM. AÇÕES: $3"
+  printf "TOTAL INVESTIDO: %9s\n" "$1"
+  printf "TOTAL ATUAL: %13s\n" "$2"
+  printf "DIFERENÇA: %15s\n" "$diferenca"  
+}
+
+function calcular_apresentar_acoes() {
+
+  #https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Shell-Parameter-Expansion
+  url="URL_"$1
+  [ "${!url}" == "" ] && {
+     echo "* Papel '$1' não existente no arquivo 'ckAcao.conf'"
+     return
+  }
+
+  url_buscar=${!url}
+  v=$(eval echo "\${${1}[*]}")
+
+  local url=`echo ${URI_PRINCIPAL}${url_buscar}`
+  local cotacao=`obter_cotacao $url`
+  local total_somatorio_compra=0
+  local total_somatorio_atual=0
+  local total_acoes=0
+
+  echo "****************************************** $1 ******************************************"
+  
+  for A in ${v[*]}; do
+     calcular_valores $total_somatorio_compra $total_somatorio_atual $total_acoes $A
+     total_somatorio_compra=`echo $retorno | cut -d';' -f1`
+     total_somatorio_atual=`echo $retorno | cut -d';' -f2`
+     total_acoes=`echo $retorno | cut -d';' -f3`
+  done
+  
+  imprimir_final $total_somatorio_compra $total_somatorio_atual $total_acoes  
+  echo ""
+}
+
+main() {
+  validar_ambiente
+  for V in `grep "URL_" ckac.conf | grep "=" | cut -d'_' -f2 | cut -d'=' -f1`; do
+     calcular_apresentar_acoes "$V"
+  done
+}
+
+main
